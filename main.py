53b0cc00e46bc83beef75b10224b1265d251b1b7
Ajay Mallepogu <ajay.mallepogu@bosframework.com>
2026-03-05 16:36:50 +0530

    added the sent sprints

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import json
+import os
+
 from services.jira_service import get_sprints_ending_today
 from services.summary_service import generate_summary
 from services.email_service import send_email
 
 
+TRACK_FILE = "sent_sprints.json"
+
+
+def load_sent_sprints():
+    if not os.path.exists(TRACK_FILE):
+        return {}
+
+    with open(TRACK_FILE, "r") as f:
+        return json.load(f)
+
+
+def save_sent_sprints(data):
+    with open(TRACK_FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+
 def run():
 
     print("Checking for sprint completion...")
+
+    sent_sprints = load_sent_sprints()
 
     sprints = get_sprints_ending_today()
 
@@ -15,6 +36,12 @@ def run():
 
     for sprint in sprints:
 
+        sprint_id = str(sprint["sprint_id"])
+
+        if sprint_id in sent_sprints:
+            print("Email already sent for:", sprint["sprint_name"])
+            continue
+
         print("Sprint ended:", sprint["sprint_name"])
 
         summary = generate_summary(sprint)
@@ -22,6 +49,11 @@ def run():
         send_email(summary)
 
         print("Email sent successfully")
+
+        # mark as sent
+        sent_sprints[sprint_id] = True
+
+    save_sent_sprints(sent_sprints)
 
 
 if __name__ == "__main__":
