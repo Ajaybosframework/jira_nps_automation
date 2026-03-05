49630af41454a4497f40e9f4d7e75697e9731647
Ajaybosframework <ajay.mallepogu@bosframework.com>
2026-03-05 17:25:29 +0530

    Update CLIENT_EMAIL to new address

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ EMAIL_SENDER = "no.replybosframework@gmail.com"
 EMAIL_PASSWORD = "vfnp nzsp exji jgwg"
 
 NPS_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSfGNYyWFBu78x1cVk8T6GfW3joTWA4hFNOLP5oNZWLwr8v6zg/viewform?usp=preview"
-CLIENT_EMAIL = "ajay.mallepogu@bosframework.com"
+CLIENT_EMAIL = "ajay.mallepogu@5yinc.com"
 
 SLACK_WEBHOOK = "https://hooks.slack.com/services/xxxxx"
